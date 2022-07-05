e06d3c56c7f2cc6248882f1f2b871674e42fa728
Sehreen22Khan <Sehreen22khan@gmail.com>
2022-07-05 16:31:39 +0530

    'final'

--- analytics/src/app/charts/charts.component.spec.ts
+++ analytics/src/app/charts/charts.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { acquireChart } from 'chartjs-test-utils';
 
 import { ChartsComponent } from './charts.component';
 
@@ -21,5 +22,37 @@ describe('ChartsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Chart Labels Test', () =>{
+      const fixture = TestBed.createComponent(ChartsComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.content span')?.textContent).toContain('analytics app is running!');
+  });
+  
+  it('should be constructed', function() {
+    var chart = acquireChart({
+      type: 'bar',
+      data: {
+        datasets: [
+          {data: []},
+          {data: []}
+        ],
+        labels: []
+      }
+    });
+
+    var meta = chart.getDatasetMeta(1);
+    expect(meta.type).toEqual('bar');
+    expect(meta.data).toEqual([]);
+    expect(meta.hidden).toBe(null);
+    expect(meta.controller).not.toBe(undefined);
+    expect(meta.controller.index).toBe(1);
+    expect(meta.xAxisID).not.toBe(null);
+    expect(meta.yAxisID).not.toBe(null);
+
+    meta.controller.updateIndex(0);
+    expect(meta.controller.index).toBe(0);
   });
 });
--- analytics/src/app/charts/charts.component.ts
+++ analytics/src/app/charts/charts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 // import { Chart, registerables } from 'chart.js';
 // Chart.register(...registerables);
 import Chart from 'chart.js/auto';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-charts',
@@ -10,7 +12,91 @@ import Chart from 'chart.js/auto';
 })
 export class ChartsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    var weekdays: any[]= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const MONTHS = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    const colorPalletes= {
+      'greens': ['#92D400', '#3DCC79', '#20A144'],
+      'reds': ['#FD554C', '#EB5A52'],
+      'blues': ['#0593FF33'],
+      'grays': ['#FFFFFF', '#F5F8FA', '#3348540D', '#EAEDF0', '#8697A8', '#303D4Ds'],
+      'fresh': ['#9BB7D4', '#1B7340', '#E9897E', '#0072B5', '#FDAC53', '#A0DAA9', '#F19828',
+        '#D2386C', '#926AA6', '#B55A30'],
+      '2-illuminating-gray': ['#949398FF', '#F4DF4EFF'],
+      '2-Coral-pacific': ['#FC766AFF', '#5B84B1FF'],
+      '2-uv-dahlia': ['#5F4B8BFF', '#E69A8DFF'],
+      '2-turquoise-sand': ['#42EADDFF', '#CDB599FF'],
+      '2-blue-orange': ['#00A4CCFF', '#F95700FF'],
+      '2-sailorblue-mint': ['#00203FFF', '#ADEFD1FF'],
+      '2-gray-lime': ['#606060FF', '#D6ED17FF'],
+      '2-tomato-rose': ['#ED2B33FF', '#D85A7FFF'],
+      '2-forest-moss': ['#2C5F2D', '#97BC62FF'],
+      '2-royalblue-peach': ['#00539CFF', '#EEA47FFF'],
+      '2-bluelemonade-aquamarine': ['#0063B2FF', '#9CC3D5FF'],
+      '2-black-yellow': ['#101820FF', '#FEE715FF'],
+      '2-palegreen-bubblegum': ['#CBCE91FF', '#EA738DFF'],
+      '2-coppercoin-agedcopper': ['#B1624EFF', '#5CC8D7FF'],
+      '2-mango-moss': ['#DAA03DFF', '#616247FF'],
+      '2-green-raspberry': ['#435E55FF', '#D64161FF'],
+      
+
+      '4-Vibrant-Pinks':['#963CBDFF', '#FF6F61FF', '#C5299BFF', '#FEAE51FF'],
+      '4-blues': ['#2460A7FF', '#85B3D1FF', '#B3C7D6FF', '#D9B48FFF'],
+      '4-golds': ['#FFD653FF', '#DDB65DFF', '#EEB238FF', '#6E4C1EFF'],
+      '4-greens': ['#0A5E2AFF', '#6DAC4FFF', '#FF4F58FF', '#FE0000FF']
+    // Soft Pink (#FFDDE2FF), Peach Amber (#FAA094FF), Yucca (#9ED9CCFF) and Arbor Green (#008C76FF)
+    // Purple (#93385FFF), Lilac (#9F6B99FF), Petunia (#4F3466FF) and Aubergine Gleam (#301728FF)
+    // Elation (#DFDCE5FF), Light Pink (#F7CED7FF), Pink (#F99FC9FF) and Dark Pink (#EF6079FF)
+    // Blue Sky (#ABD1C9FF), Elation (#DFDCE5FF), Nugget (#DBB04AFF) and Celestial (#97B3D0FF)
+    // Desert Sand (#DBBEA1FF) Burnished Brown (#A37B73FF) Old Burgundy (#3E282BFF) and Mystic (#D34F73FF)
+    // Vermilion (#E3170AFF) Celadon (#A9E5BBFF) Medium Champagne (#FCF6B1FF) and Honey Yellow (#F7B32BFF)
+    // Sapphire (#2E5266FF) Light Slate Gray (#6E8898FF) Cadet Gray (#9FB1BCFF) and American Silver (#D3D0CBFF)
+    }
+    
+    // Rose Pink (#DF6589FF) and Purple (#3C1053FF)
+    // Royal Purple (#603F83FF) and Ice Flow (#C7D3D4FF)
+    // Mellow Yellow (#FFE77AFF) and Verdant Green (#2C5F2DFF)
+    // Lemon Tonic (#FCF951FF) and Purple (#422057FF)
+    // Teal (#4B878BFF) and Fiery Red (#D01C1FFF)
+    // Nebulosity (#1C1C1BFF) and Pink Yarrow (#CE4A7EFF)
+
+
+    // Blazing Orange (#FFA351FF), Buff Orange (#FFBE7BFF) and Yellow Cream (#EED971FF)
+    // Red (#DA291CFF), Norse Blue (#56A8CBFF) and Light Green (#53A567FF)
+    // Light Purple (#D7A9E3FF), Light Blue (#8BBEE8FF) and Light Green (#A8D5BAFF)
+    // Radiant Yellow (#F9A12EFF), Living Coral (#FC766AFF) and Purple (#9B4A97FF)
+    // Warm Gray (#A59C94FF), Crimson (#AE0E36FF) and Raspberry (#D32E5EFF)
+    // Living Coral (#FC766AFF), Spiced Apple (#783937FF) and Peach (#F1AC88FF)
+    // Lemon Verbena (#F6EA7BFF), Orange Pop (#FFBA52FF) and Aurora Pink (#E683A9FF)
+    // Tanager Turquoise (#95DBE5FF), Teal Blue (#078282FF) and Kelly Green (#339E66FF)
+    // Knockout Pink (#FF3EA5FF), Safety Yellow (#EDFF00FF) and Out of the Blue (#00A4CCFF)
+
+    // Bright Violet (#963CBDFF), Living Coral (#FF6F61FF), Vibrant Pink (#C5299BFF) and Marigold (#FEAE51FF)
+    // Deep Blue (#2460A7FF), Northern Sky (#85B3D1FF), Baby Blue (#B3C7D6FF) and Coffee (#D9B48FFF)
+    // Habañero Gold (#FFD653FF), Dijon (#DDB65DFF), Honey (#EEB238FF) and Chestnut (#6E4C1EFF)
+    // Dark Green (#0A5E2AFF), Light Green (#6DAC4FFF), Fiery Coral (#FF4F58FF) and Red (#FE0000FF)
+    // Soft Pink (#FFDDE2FF), Peach Amber (#FAA094FF), Yucca (#9ED9CCFF) and Arbor Green (#008C76FF)
+    // Purple (#93385FFF), Lilac (#9F6B99FF), Petunia (#4F3466FF) and Aubergine Gleam (#301728FF)
+    // Elation (#DFDCE5FF), Light Pink (#F7CED7FF), Pink (#F99FC9FF) and Dark Pink (#EF6079FF)
+    // Blue Sky (#ABD1C9FF), Elation (#DFDCE5FF), Nugget (#DBB04AFF) and Celestial (#97B3D0FF)
+    // Desert Sand (#DBBEA1FF) Burnished Brown (#A37B73FF) Old Burgundy (#3E282BFF) and Mystic (#D34F73FF)
+    // Vermilion (#E3170AFF) Celadon (#A9E5BBFF) Medium Champagne (#FCF6B1FF) and Honey Yellow (#F7B32BFF)
+    // Sapphire (#2E5266FF) Light Slate Gray (#6E8898FF) Cadet Gray (#9FB1BCFF) and American Silver (#D3D0CBFF)
+    
+   }
 
   ngOnInit(): void {
     this.Appointments();
@@ -29,9 +115,28 @@ export class ChartsComponent implements OnInit {
   }
   }
 
+  public captureScreen()  
+  {  
+    console.log("fn called!")
+    var data = document.getElementById('pdf')!;
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png');
+      // let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      const pdf = new jsPDF();
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    });  
+  } 
+
   private labels_count_arrays(data: any, labels_field: string){
     var labels: any[] = [];
-    var counts: any[] = [];
     var item;
     for(var i=0;i<data.length;i++){
       item = data[i][labels_field]
@@ -111,6 +216,10 @@ export class ChartsComponent implements OnInit {
     ]
     const label_count = this.labels_count_arrays(api_data,"visit_type");
     console.log(label_count);
+    const y_prefix = '$';
+    const y_suffix = 'min';
+    const x_title = 'Visit Types';
+    const y_title = 'Count';
     // const data = {
     //   labels: Object.keys(label_count),
     //   datasets: [{
@@ -156,11 +265,42 @@ export class ChartsComponent implements OnInit {
         }]
       },
       options: {
+        scales: {
+          y: {
+            ticks: {
+                // Include a dollar sign in the ticks
+                callback: function(value, index, values) {
+                    return y_prefix + ' ' + value + ' ' + y_suffix;
+                }
+            },
+            title: {
+              display: true,
+              text: y_title
+            },
+            grid: {
+                display: false
+            }
+          },
+          x: {
+            title: {
+              display: true,
+              text: x_title
+            },
+            grid: {
+                display: false
+            }
+          }
+        },
         plugins:{
           legend: {display: false},
           title: {
             display: true,
             text: "Appointments by Visit Type"
+          },
+          tooltip: {
+            enabled: true,
+            position: 'average',
+            
           }
         }
       }
